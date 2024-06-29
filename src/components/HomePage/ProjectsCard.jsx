b5ec2aa7b215f8a79627e2dcf0a458a285d8a2b7
RakibHossen678 <rh491464@gmail.com>
2024-06-29 22:17:41 +0600

    fixed responsive related error

--- src/components/HomePage/ProjectsCard.jsx
+++ src/components/HomePage/ProjectsCard.jsx
@@ -5,8 +5,9 @@ import React from "react";
 const ProjectsCard = ({ item }) => {
   const { title, description, image, link, github } = item;
   return (
-    <div className="lg:group">
-      <div className="lg:max-w-lg lg:h-[460px] overflow-hidden bg-white rounded-lg shadow-md ">
+    <div className="lg:group flex justify-center item-center">
+      <div className="max-w-sm md:max-w-md lg:max-w-lg lg:h-[460px] overflow-hidden  bg-white rounded-lg shadow-md ">
+      
         <Image
           className=" bg-cover lg:group-hover:scale-110  bg-center  lg:h-64"
           src={image}
